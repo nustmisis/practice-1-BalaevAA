3044159992cc28b5094dd11e9ae83c8144cee235
Anton <balaevantoine@gmaiil.com>
2023-09-27 15:59:53 +0300

    add try/except

--- practice_1/task_3.py
+++ practice_1/task_3.py
@@ -5,8 +5,22 @@ BMI) человека. Пользователь должен ввести сво
 используете одну из приведенных ниже формул для определения индекса.
 BMI = вес/рост**2 
 """
-weight = int(input())
-height = float(input())
-BMI = weight / height**2
-print(BMI)
+try:
+    weight = float(input())
+    height = float(input())
+except ValueError:
+    print("Ошибка ввода данных")
 
+BMI = weight / height**2
+print(f'BMI = {BMI}')
+
+if BMI < 18.5:
+    t = "Дефицит"
+elif 18.5 <= BMI < 24.9:
+    t = "Норма"
+elif 25 <= BMI < 29.9:
+    t = "Предожирение"
+else:
+    t = "Ожирение"
+
+print(f'Категория BMI: {t}')
